29f219f4ea442cf6127c795ca3045c65d37f2115
Timo Reichl <thetredev@gmail.com>
2023-12-27 23:46:54 +0100

    day-2 / part-1: Add C#
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

---
+++ day-2/part-1/02-csharp/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace aoc_day2;
+
+
+public static class Program
+{
+    private static readonly Dictionary<string, int> MAX_CUBES = new()
+    {
+        { "red", 12 },
+        { "green", 13 },
+        { "blue", 14 },
+    };
+
+    private static bool IsPossible(string[] groups)
+    {
+        foreach (string group in groups)
+        {
+            string[] slices = group.Split(", ");
+
+            foreach (string value in slices)
+            {
+                string[] slice = value.Split(' ');
+                string cubes = slice[0];
+                string color = slice[1];
+
+                if (cubes.EndsWith('\n'))
+                {
+                    cubes = cubes[..^-1];
+                }
+
+                if (int.Parse(cubes) > MAX_CUBES[color])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        int currentGameId = 0;
+
+        Console.WriteLine(File.ReadLines(args[0]).Sum(line =>
+        {
+            currentGameId++;
+
+            var data = line[(line.IndexOf(':') + 2)..];
+            var groups = data.Split("; ");
+
+            return IsPossible(groups) ? currentGameId : 0;
+        }));
+    }
+}

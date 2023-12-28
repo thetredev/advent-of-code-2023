922b0ad65a752d27f49d04507143eea0f19eb2d5
Timo Reichl <thetredev@gmail.com>
2023-12-28 12:29:50 +0100

    day-01 / rust: clippy + rustfmt
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- day-01/07-rust/main.rs
+++ day-01/07-rust/main.rs
@@ -4,9 +4,7 @@ fn chars_to_int(chars: &Vec<char>) -> i32 {
     let first: char = *chars.first().unwrap();
     let last: char = *chars.last().unwrap();
 
-    format!("{}{}", first, last)
-        .parse::<i32>()
-        .unwrap()
+    format!("{}{}", first, last).parse::<i32>().unwrap()
 }
 
 fn main() {
@@ -16,9 +14,14 @@ fn main() {
         .unwrap()
         .lines()
         .map(String::from) // single line as string
-        .map(|line| chars_to_int(&line.chars() // first and last char as i32
-            .filter(|c| c.is_digit(10))
-            .collect::<Vec<char>>()))
+        .map(|line| {
+            chars_to_int(
+                &line
+                    .chars() // first and last char as i32
+                    .filter(|c| c.is_digit(10))
+                    .collect::<Vec<char>>(),
+            )
+        })
         .sum(); // sum of all findings
 
     println!("{}", result);

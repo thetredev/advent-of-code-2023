d34278865720c7c57bfc277e871f3c849bd125a2
Timo Reichl <thetredev@gmail.com>
2023-12-28 10:30:47 +0100

    day-01 / rust: Get rid of mutable temp variable via function chars_to_int()
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- day-01/07-rust/main.rs
+++ day-01/07-rust/main.rs
@@ -1,5 +1,14 @@
 use std::fs::read_to_string;
 
+fn chars_to_int(chars: &Vec<char>) -> i32 {
+    let first: char = *chars.first().unwrap();
+    let last: char = *chars.last().unwrap();
+
+    format!("{}{}", first, last)
+        .parse::<i32>()
+        .unwrap()
+}
+
 fn main() {
     let filename = std::env::args().nth(1).expect("no input file given");
     let lines: Vec<String> = read_to_string(filename)
@@ -8,20 +17,11 @@ fn main() {
         .map(String::from)
         .collect();
 
-    let mut result: i32 = 0;
-
-    for line in lines.iter() {
-        let digits: Vec<char> = line.chars()
+    let result: i32 = lines.iter()
+        .map(|line| chars_to_int(&line.chars()
             .filter(|c| c.is_digit(10))
-            .collect();
-
-        let first: char = *digits.first().unwrap();
-        let last: char = *digits.last().unwrap();
-
-        result += format!("{}{}", first, last)
-            .parse::<i32>()
-            .unwrap();
-    }
+            .collect::<Vec<char>>()))
+        .sum();
 
     println!("{}", result);
 }
